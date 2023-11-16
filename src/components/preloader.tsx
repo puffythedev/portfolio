cced3485ccbc4aa87f3e869d2851bc41c51b888e
SkyOPG <nicknameopg1@gmail.com>
2023-11-16 23:59:41 +0100

    W rizz

---
+++ src/components/preloader.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { useEffect, useState } from 'react'
+
+function Preloader() {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        const handleLoad = () => {
+            setLoaded(true);
+        }
+
+        window.addEventListener('load', handleLoad)
+
+        return () => {
+            window.removeEventListener('load', handleLoad)
+        }
+    }, []);
+
+    return (
+        <div className= {`preloader ${loaded ? 'loaded' : ''}`}>
+            {!loaded && (
+                <div className="border">
+                  <div className="notch" />
+                  <div className="water" />
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Preloader
