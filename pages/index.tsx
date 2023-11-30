cd8b5edc25d454c6721b09083c4a8a7c70cb074f
SkyOPG <nicknameopg1@gmail.com>
2023-11-30 23:26:55 +0000

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 // pages/index.jsx
 import React from 'react';
-import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import AboutMe from '../components/AboutMe';
@@ -8,13 +7,6 @@ import Footer from '../components/Footer';
 
 const Home = () => (
   <div className="bg-black text-white">
-    <Head>
-      <title>Your Name - Web Developer</title>
-      <meta
-        name="description"
-        content="Portfolio of Your Name, a passionate web developer specializing in UI/UX Design, Responsive Web Design, and Backend Development."
-      />
-    </Head>
 
     <Navbar />
     <Hero />
--- shared/shared.ts
+++ shared/shared.ts
@@ -1,3 +1,4 @@
-import { Inter } from 'next/font/google'
+import { Inter, Noto_Sans } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] });
+export const noto = Noto_Sans({ weight: "100", subsets: ['latin'] })
