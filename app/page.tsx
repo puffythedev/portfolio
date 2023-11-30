84e0b1234c671326011572d2b024714cba14b3c5
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2023-11-30 21:43:26 +0000

    chore: Attempt to fix terrible code

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
-// App.js
+// pages/index.jsx
 import React from 'react';
-import './App.css'; // Include your Tailwind CSS file
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import AboutMe from '../components/AboutMe';
-import Footer from '../components/Footer';
+import Head from 'next/head';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import AboutMe from './components/AboutMe';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="bg-black text-white">
+const Home = () => (
+  <div className="bg-black text-white">
+    <Head>
+      <title>Your Name - Web Developer</title>
+      <meta
+        name="description"
+        content="Portfolio of Your Name, a passionate web developer specializing in UI/UX Design, Responsive Web Design, and Backend Development."
+      />
+    </Head>
 
-      <Navbar />
-      <Hero />
-      <AboutMe />
-      <Footer />
-    </div>
-  );
-}
+    <Navbar />
+    <Hero />
+    <AboutMe />
+    <Footer />
+  </div>
+);
 
-export default App;
+export default Home;
