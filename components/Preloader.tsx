86c4f4cb221322649df57db96f913986c8322704
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2023-12-01 09:53:10 +0000

    chore: fix prelader

--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -17,9 +17,13 @@ const Preloader: React.FC = () => {
       className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${
         showPreloader ? 'visible' : 'invisible'
       }`}
+      style={{
+        backgroundColor: 'rgba(0, 0, 0, 0.8)', 
+        zIndex: 9999, 
+      }}
     >
       <img
-        src="https://media.kars.bio/assets/pfp.gif"
+        src="https://media.kars.bio/assets/pfp.gif" // Replace with the path to your loader image
         alt="Preloader"
         className="rounded-full"
         style={{ width: '50px', height: '50px' }}
