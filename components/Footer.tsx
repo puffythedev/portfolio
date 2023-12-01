eddfa65b48ad038ccc444d208edd1154885517be
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2023-12-01 07:59:12 +0000

    chore: Change Footer Credit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="text-center py-4">
-      <p>&copy; 2023 Portfolio. All rights reserved. Site by <a href="https://kars.bio">Kars</a></p>
+      <p>&copy; 2023 SkyOPG. All rights reserved.
+        <br />
+       Site by <a href="https://kars.bio" className="text-orange-300">Kars</a></p>
     </footer>
   );
 };
