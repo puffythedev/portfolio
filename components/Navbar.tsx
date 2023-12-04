4466e38f1ce557a9d69c833623b2bc6a0455ab01
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2023-12-04 21:14:51 +0000

    chore

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-black p-4">
+    <nav className="backdrop-filter backdrop-blur-md fixed p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white font-bold text-xl">SkyOPG</div>
         <div className="lg:hidden">
