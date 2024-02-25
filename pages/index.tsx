aff499d138e2dc48f3c062b6434a75c36c658ad0
SkyOPG <nicknameopg1@gmail.com>
2024-02-25 11:11:01 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,39 @@ import Projects from '@/components/Projects'
 import Head from 'next/head';
 import Title from '@/components/Title';
 import Kon from '@/components/Console';
+import Cards from '@/components/Testimonials2'
 
 function Home(){
+  const testimonials = [
+    {
+		id: 0,
+		quote: "i luh ma bf🤪",
+		name: "Khloe",
+		title: "Sky's wife",
+		img: "https://cdn.discordapp.com/avatars/1129112487900491876/5f0136a9e29c6c206ff6f5bba181cc06.webp"
+	},
+	{
+      id: 1,
+      quote: "always sky, always sky; boyfriends can be gay and girlfriends can be lesbian",
+      name: "Julian",
+      title: "XenoFM owner",
+	  img: "https://cdn3.kars.bio/assets/external/sky_1.webp"
+    },
+	{
+		id: 2,
+		quote: "I eat animal's hearts\n",
+		name: "Yastube",
+		title: "TeamEnvex Owner",
+		img: "https://cdn3.kars.bio/assets/external/sky_2.webp"
+	},
+	{
+		id: 3,
+		quote: "Hi\n",
+		name: "notbangbang",
+		title: "Pro MC gamer",
+		img: "https://cdn.discordapp.com/attachments/1173954453045514240/1181351258242678895/t3WSIkm.png"
+	}
+  ];
   return (
     <div className="bg-black text-white">
       <Head>
@@ -23,7 +54,7 @@ function Home(){
       <Navbar />  
       <Hero />
       <AboutMe />
-      <Testimonials />
+      <Cards items={testimonials} />
       <Title title="Projects" desc="Check out my latest work!"/>
       <Projects />
       <Footer />
