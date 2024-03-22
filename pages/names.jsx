1754822e4d17568e99d7f3c62b67fa774e8f2d31
Amine <NicknameOPG1@gmail.com>
2024-03-22 15:37:23 +0000

    Update names.jsx

--- pages/names.jsx
+++ pages/names.jsx
@@ -9,7 +9,8 @@ export default function SkyScript(){
         <meta name='title' content='SkyOPG.pro - Hall of names' />
         <meta name='description' content="just a cool idea idk" />
       </Head> <Navbar /> <Title title='' desc='' /> <Title title='Hall of names' desc='dm "skyopg." in discord to get a part here' />
-            <Title title='yo name here' desc={'description 🤯'} />
+            <Title title='jaydennxo' desc={'cute person'} />
+            <Title title='skyopg.' desc={'u better talk to me or i be sad'} />
         </>
     )
 }
