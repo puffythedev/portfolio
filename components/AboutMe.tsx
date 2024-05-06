c182d04bd202766f99380652a261e52a55e92d52
Amine <NicknameOPG1@gmail.com>
2024-05-06 13:23:34 +0100

    Update AboutMe.tsx

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // Passionate Developer, Creative Mind, Tech Enthusiast
-const AboutMe = ({ traits }) => {
+const AboutMe = ({ traits }: {traits: string}) => {
   const idk = traits.trim().slice(", ")
   return (
     <section className="container mx-auto text-center py-12">
